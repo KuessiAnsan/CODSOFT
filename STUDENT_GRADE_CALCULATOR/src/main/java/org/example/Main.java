c5ef112d9b2171bee89bf07545d1b01bd4b02441
KuessiAnsan <kuessiansandjiyehoue@gmail.com>
2024-01-02 11:56:18 +0000

    Handling exceptions in STUDENT GRADE CALCULATOR

--- STUDENT_GRADE_CALCULATOR/src/main/java/org/example/Main.java
+++ STUDENT_GRADE_CALCULATOR/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,33 +9,41 @@ import static java.util.List.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("How many subjects: ");
+        System.out.println("WELCOME TO THE GRADE CALCULATOR");
 
-        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("How many subjects: ");
 
-        int subjects = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
 
-        List<Subject> subjectList = of(
-                new Subject("Maths"),
-                new Subject("Physics"),
-                new Subject("History"),
-                new Subject("Geography"),
-                new Subject("English"),
-                new Subject("French")
-        );
-        double marksSum = 0;
+            int subjects = scanner.nextInt();
 
-        for (int i=1; i<=subjects; i++) {
-            System.out.println("Mark for subject-" + i + ":");
-            Scanner scanner1 = new Scanner(System.in);
-            double subjectMark = scanner1.nextDouble();
-            marksSum+=subjectMark;
+            List<Subject> subjectList = of(
+                    new Subject("Maths"),
+                    new Subject("Physics"),
+                    new Subject("History"),
+                    new Subject("Geography"),
+                    new Subject("English"),
+                    new Subject("French")
+            );
+            double marksSum = 0;
+
+            for (int i=1; i<=subjects; i++) {
+                System.out.println("Mark for subject-" + i + ":");
+                Scanner scanner1 = new Scanner(System.in);
+                double subjectMark = scanner1.nextDouble();
+                marksSum+=subjectMark;
+            }
+
+            double averagePercentage = marksSum / subjectList.size();
+
+
+            System.out.println("Total of subjects: " + subjects + '\n'+ "Sum of marks: " + marksSum + '\n'+ "Average percentage: " + averagePercentage + "%" + '\n');
+        }catch (InputMismatchException exception){
+            System.out.println("You have to enter a number");
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
         }
-
-        double averagePercentage = marksSum / subjectList.size();
-
-
-        System.out.println("Total of subjects: " + subjects + '\n'+ "Sum of marks: " + marksSum + '\n'+ "Average percentage: " + averagePercentage + "%" + '\n');
 
     }
 }
